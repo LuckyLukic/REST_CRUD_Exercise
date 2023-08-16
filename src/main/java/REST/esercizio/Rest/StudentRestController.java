1caee775c455bd6bc7d06379c464a128e871aef9
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-08-17 01:15:24 +0200

    generic exception handler

--- src/main/java/REST/esercizio/Rest/StudentRestController.java
+++ src/main/java/REST/esercizio/Rest/StudentRestController.java
@@ -73,4 +73,20 @@ public class StudentRestController {
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
 	
+	//Add generic exception handler
+	
+		@ExceptionHandler
+		public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+			
+			//Create a StudentErrorResponse
+			
+			StudentErrorResponse error = new StudentErrorResponse();
+			
+			error.setStatus(HttpStatus.BAD_REQUEST.value());
+			error.setMessage(exc.getMessage());
+			error.setTimeStamp(System.currentTimeMillis());
+			
+			return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		}
+	
 }
