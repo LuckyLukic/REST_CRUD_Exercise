011884e2106391fdbe4155630b982d79410a4b89
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-08-17 00:17:30 +0200

    refactoring studentRest controller

--- src/main/java/REST/esercizio/Rest/StudentRestController.java
+++ src/main/java/REST/esercizio/Rest/StudentRestController.java
@@ -8,22 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import Entity.Student;
+import jakarta.annotation.PostConstruct;
 
 
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	private List<Student> theStudents;
+	
+	//@PostConstruct to load the student data only once
+	@PostConstruct
+	public void loadData() {
+		
+        theStudents = new ArrayList<>();
+		
+		theStudents.add(new Student("Luca", "Iannice"));
+		theStudents.add(new Student("Ari", "Ki"));
+		theStudents.add(new Student("Ciccio", "Cicciooo"));
+		
+	}
 
 	
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 		
-		List<Student> theStudents = new ArrayList<>();
 		
-		theStudents.add(new Student("Luca", "Iannice"));
-		theStudents.add(new Student("Ari", "Ki"));
-		theStudents.add(new Student("Ciccio", "Cicciooo"));
 		
 		return theStudents;
 	}
